60677f839af8874607960847a5c931907e1037a6
iTwicKz <twickes32@ufl.edu>
2016-10-09 11:05:41 -0400

    Completed dock image

--- pillwatch/pillwatch/PrescriptionDetailsViewController.swift
+++ pillwatch/pillwatch/PrescriptionDetailsViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class PrescriptionDetailsViewController: UIViewController {
     
@@ -24,14 +26,21 @@ class PrescriptionDetailsViewController: UIViewController {
     @IBOutlet weak var drugImageView: UIImageView!
     @IBOutlet weak var progressPillsView: UIView!
     @IBOutlet weak var pillImage: UIImageView!
+    @IBOutlet weak var dockimage: UIImageView!
 
+    @IBOutlet weak var noticationView: UIView!
+    @IBOutlet weak var notificationLabel: UILabel!
     var listedViewController: PrescriptionsListViewController?
     var medicationDetails: [String:String]? = [:]
     var warningsURL: NSURL?
     var monthsFull: [String] = ["January", "Februaru", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    var particlePressureEvents: JSON = []
+//    var helloWorldTimer = NSTimer.scheduledTimerWithTimeInterval(2.0, target: self, selector: Selector("isPressureLoaded"), userInfo: nil, repeats: true)
+
     
     
     override func viewWillAppear(animated: Bool) {
+        self.dockimage.hidden = true
         self.nameLabel.text = passedPrescription.name
         self.remainingLabel.text = String(passedPrescription.remainingCount!)
         self.totalLabel.text = "/ " + String(passedPrescription.totalCount!)
@@ -67,8 +76,55 @@ class PrescriptionDetailsViewController: UIViewController {
         return "due by " + listedViewController!.formatTime(newTime) + dayText
     }
     
+    func isPressureLoaded(){
+        
+        if(passedPrescription.name == "Naproxen"){
+            
+            let endPoint: String = "https://api.thingspeak.com/channels/168751/feeds.json?"
+            var isPressured = false
+
+            Alamofire.request(.GET, endPoint).responseJSON { response in
+                if let JSON = response.result.value {
+                    let response = JSON as! NSDictionary
+                    let totalResponse = response["feeds"] as! [NSDictionary]
+                    for i in 1...5 {
+                        var tempDict = totalResponse[totalResponse.count - i]
+                        print(tempDict)
+    //                    print(tempDict["field1"])
+                        if(String(tempDict["field1"]!) == "Loaded"){
+                            isPressured = true
+                            print("ASIDSAIOJFIASJFILASJ")
+                            break
+                        }
+                        
+                    }
+                    if(isPressured){
+                        self.notificationLabel.text = "Docked"
+    //                    self.notificationLabel.textColor = UIColor(red:0.57, green:0.90, blue:0.19, alpha:1.0)
+                        self.noticationView.backgroundColor = UIColor(red:0.81, green:1.00, blue:0.63, alpha:1.0)
+                        self.dockimage.hidden = false
+
+                        
+                    } else {
+                        self.notificationLabel.text = "Released"
+                        //                    self.notificationLabel.textColor = UIColor(red:0.57, green:0.90, blue:0.19, alpha:1.0)
+                        self.noticationView.backgroundColor = UIColor(red:1.00, green:0.29, blue:0.29, alpha:1.0)
+                        self.dockimage.hidden = true
+                    }
+                    print("HELLO: " + String(isPressured))
+
+    //                print(totalResponse[totalResponse.count-1])
+                }
+            }
+        }
+        
+//        return isPressured
+
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         medicationDetails = (listedViewController?.getMedicationItem(passedPrescription.name!))!
         print(medicationDetails)
         if(medicationDetails != nil){
@@ -85,6 +141,17 @@ class PrescriptionDetailsViewController: UIViewController {
         self.nameView.layer.shadowOffset = CGSize(width: 0, height: 1)
         self.nameView.layer.shadowOpacity = 0.25
         
+        
+        self.noticationView.layer.cornerRadius = 4
+        
+        self.noticationView.layer.shadowColor = UIColor(red:0.57, green:0.90, blue:0.19, alpha:1.0).CGColor
+
+        self.noticationView.layer.shadowRadius = 2.5
+        self.noticationView.layer.shadowOffset = CGSize(width: 0, height: 1)
+        self.noticationView.layer.shadowOpacity = 0.35
+        
+        
+        
         self.doseView.layer.cornerRadius = 4
         self.doseView.layer.shadowColor = UIColor(red:0.42, green:0.69, blue:1.00, alpha:1.0).CGColor
         self.doseView.layer.shadowRadius = 2.5
@@ -92,6 +159,8 @@ class PrescriptionDetailsViewController: UIViewController {
         self.doseView.layer.shadowOpacity = 0.25
         
         // Do any additional setup after loading the view.
+        self.isPressureLoaded()
+
     }
 
     override func didReceiveMemoryWarning() {
--- pillwatch/pillwatch/PrescriptionsListViewController.swift
+++ pillwatch/pillwatch/PrescriptionsListViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import SwiftyJSON
+import Alamofire
 
 class PrescriptionsListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
